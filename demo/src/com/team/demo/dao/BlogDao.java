e73bf6cd8c200a464adc09cf413151e94bb06503
24459 <24459@DESKTOP-8OJHD9Q>
2020-12-31 11:42:18 +0800

    提交xxxx

---
+++ demo/src/com/team/demo/dao/BlogDao.java
@@ -0,0 +1,19 @@
+package com.team.demo.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import com.team.demo.util.DBHelper;
+
+public class BlogDao {
+	public List<?> query(){
+		String sql = "select * from topic where tid = " + tid;
+		try {
+			return DBHelper.selectListMap(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+	}
+}
